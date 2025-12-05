06a34c92a46875300e1b1abe039ec16f6d2e7b29
radin-111 <radinsheikh87@gmail.com>
2025-12-05 14:47:28 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is running");
+});
+export default app;
