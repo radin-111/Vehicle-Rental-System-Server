6c538b1711f6df30a8d6ec98bb0322fda5dca1a4
radin-111 <radinsheikh87@gmail.com>
2025-12-05 21:03:45 +0600

    making another tables

--- src/config/db.ts
+++ src/config/db.ts
@@ -17,4 +17,28 @@ export const initDB = async () => {
         )
         
         `);
+
+  await pool.query(`
+      CREATE TABLE IF NOT EXISTS vehicles(
+      id SERIAL PRIMARY KEY,
+      vehicle_name VARCHAR(100) NOT NULL,
+      type TEXT NOT NULL CHECK (type in('car', 'bike', 'van' , 'SUV')),
+      registration_number TEXT NOT NULL UNIQUE,
+      daily_rent_price INT NOT NULL CHECK (daily_rent_price > 0),
+      availability_status TEXT NOT NULL CHECK (availability_status in('available', 'booked'))
+      )
+      `);
+
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS bookings(
+      id SERIAL PRIMARY KEY,
+      customer_id INT REFERENCES users(id) ON DELETE CASCADE,
+      vehicle_id INT REFERENCES vehicles(id) ON DELETE CASCADE,
+      rent_start_date	DATE NOT NULL,
+      rent_end_date DATE NOT NULL CHECK (rent_end_date > rent_start_date),
+      total_price INT NOT NULL CHECK (total_price > 0),
+      status VARCHAR(10) NOT NULL CHECK (status IN ('active', 'cancelled', 'returned'))
+        
+      )
+      `)
 };
